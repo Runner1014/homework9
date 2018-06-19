1980343944efa42d367782372d2a0611c7808e4a
Runner1014 <31625057+Runner1014@users.noreply.github.com>
2018-06-19 22:47:06 +0800

    Add files via upload

---
+++ Assert/Scripts/Bake.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Bake : MonoBehaviour {
+    private NavMeshAgent tank;
+    public Transform target;
+	// Use this for initialization
+	void Start () {
+        tank = gameObject.GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        tank.SetDestination(target.position);
+    }
+}
---
+++ Assert/Scripts/SSDirector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SSDirector : System.Object
+{
+    private static SSDirector instance;
+    public SceneController currentSceneController { get; set; }
+
+    private SSDirector() { }
+
+    public static SSDirector getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new SSDirector();
+        }
+        return instance;
+    }
+}
